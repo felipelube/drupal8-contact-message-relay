8fbc09d701132f0709daf27253a42fa3d0627615
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2017-11-20 17:27:46 -0200

    use Flask's config.from_object, remove unneeded funcs params, refactor lbyl to eafp

--- src/app.py
+++ src/app.py
@@ -2,7 +2,6 @@
 An small API for relaying contact messages to Drupal after server-side Recaptcha validation.
 '''
 import logging
-import secrets
 from exceptions import RecaptchaError, APIException
 import jsend
 import requests
@@ -14,39 +13,44 @@ from jsonschema import ValidationError, validate
 from schemas import CONTACT_FORM_SCHEMA
 
 APP = Flask(__name__)
-CORS(APP)
+APP.config.from_object('config')
 
-def do_recaptcha_validation(secret, response):
-    ''' Perform a server-side Google ReCaptcha validation '''
+def do_recaptcha_validation(response):
+    ''' Perform a server-side Google ReCaptcha validation '''    
     with requests.post('https://www.google.com/recaptcha/api/siteverify', data={
-        "secret": secret,
+        "secret": APP.config["RECAPTCHA_SITE_KEY"],
         "response": response
     }) as req:
         req.raise_for_status()
-        if req.json()['success'] != True:
-            raise RecaptchaError("Non valid Recaptcha", req.json()['error_codes'])
+        json_response = req.json()
+        try:            
+            assert json_response['success'] == True
+        except AssertionError:
+            raise RecaptchaError("Invalid", json_response['error-codes'])        
+        else:
+            return True
 
-def post_drupal_contact_message(contact, form_id):
+def post_drupal_contact_message(contact):
     ''' Post contact message to Drupal '''
-    with requests.post(secrets.DRUPAL_CONTACT_MESSAGE_URL, json={
-        "contact_form": form_id,
+    with requests.post(app.config["DRUPAL_CONTACT_MESSAGE_URL"], json={
+        "contact_form": APP.config["DRUPAL_CONTACT_FORM_ID"],
         "message": [contact["message"]],
         "subject": ["Contato - " + contact["name"]],
         "mail": [contact["mail"]],
         "name": [contact["name"]]
-    }, auth=(secrets.DRUPAL_AUTH_USER, secrets.DRUPAL_AUTH_PASSWORD)) as req:
+    }, auth=(app.config["DRUPAL_AUTH_USER"], app.config["DRUPAL_AUTH_PASSWORD"])) as req:
         req.raise_for_status()
         return req.json()
 
 @APP.route('/', methods=['POST'])
-def handle_form():
+def handle_form():    
     ''' Web entrypoint '''
     try:
-        assert request.is_json
+        assert request.is_json        
         received_data = request.get_json()
         validate(received_data, CONTACT_FORM_SCHEMA)
-        do_recaptcha_validation(secrets.RECAPTCHA_SITE_KEY, received_data["recaptcha_response"])
-        drupal_response = post_drupal_contact_message(received_data, secrets.DRUPAL_CONTACT_FORM_ID)
+        do_recaptcha_validation(received_data["recaptcha_response"])
+        drupal_response = post_drupal_contact_message(received_data)
         response = APP.response_class(
             response=json.dumps(jsend.success({'data': drupal_response})),
             status=201,
